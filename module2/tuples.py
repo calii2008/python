19f382b5f6fd44a1aa5b4055d8a5df90aad6367d
Student <ensarsahiti60@gmail.com>
2025-10-28 16:02:35 +0100

    module2

---
+++ module2/tuples.py
@@ -0,0 +1,25 @@
+# words=("spam","white","black")
+#
+# person=("alice","30",'engineer')
+#
+# name,age,profession=person
+#
+# print(name, "'s", "profession is",profession,"and she is",age,"years old")
+from operator import concat
+
+contact_info={
+    "alice":"555-12354",
+    "bob":"55544843"
+}
+
+alice_phone=contact_info['alice']
+print['daris']='044471154'
+
+print(contact_info)
+
+del contact_info[]
+
+contact_info{
+    "alice"
+
+}
