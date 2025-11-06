7536cfacadac7303198c9b185a110d082f13658e
calii2008 <ensarsahiti60@gmail.com>
2025-11-06 15:58:21 +0100

    module 3

---
+++ module3/set.py
@@ -0,0 +1,102 @@
+# my_set={1,2,3}
+#
+# my_set=set()
+#
+# my_set=set([4,5,6])
+#
+# print(my_set)
+#
+# set1 = {1, 2, 3,}
+#
+# set2 = {3, 4, 5,}
+#
+# union_result_method = set1.union(set2)
+#
+# print("union: ", union_result_method)
+#
+# union = set1 | set2
+# print("union method: ", union)
+#
+# intersection = set1 | set2
+# print("intersection method: ", intersection)
+#
+# difference = set1 | set2
+# print("difference method: ", difference)
+#
+# symmetrical = set1 | set2
+# print("symmetrical_method: ", symmetrical)
+#
+#
+#
+# my_set = {1, 2, 3}
+#
+# my_set.add(4)
+#
+# my_set.remove(3)
+# my_set.discard(1)
+# my_set.clear()
+#
+# print (my_set)
+#
+# set_length = len(my_set)
+# print("length of se")
+
+# my_list = [1 , 2 ,3  4, 5, 5,]
+#
+# unique_set = set(set_list)
+#
+# unique_list = list(unique_set)
+#
+# print(unique_list)
+
+# loyalty_members = {"melika" , "ensar", "shpat"}
+# costomer = "melika"
+#
+# is_member = customer in loyalty_members
+#
+# print(is_member)
+
+# age = 18
+#
+# if age >= 18:
+#     print("you can vote")
+# else:
+#     print("you cant vote")
+#
+# student_gpa >= 3.5:
+#     print(f"Studenta me gpa {student_gpa} and test score of {student_score} may be eligible for a partila scholarship")
+#     elif student_score <= 65:
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    = {1, 2, 3}
+
+print 
+
+
+
+
+
+
+
+
+
+
+
