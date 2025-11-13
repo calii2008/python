dd988f3d8a7dea42575f247f68ac16ca923d8ce1
ensar <ensarsahiti60@gmail.com>
2025-11-13 15:59:40 +0100

    a

---
+++ module5/funcion.py
@@ -0,0 +1,44 @@
+# def green():
+#     print("pershendetje")
+#
+# greet()
+#
+# def greet_person(name):
+#     print("hello",name):
+#
+# greet_person("daris")
+# green_person("gent")
+from pyexpat.errors import messages
+
+
+# def greet(name):
+#     message=f"hello.{name}"
+#     print(massage)
+#
+# greet("alice")
+# print(message)
+
+# greeting="hello"
+#
+# def greet(name):
+#     message=f"{}"
+#
+# greet("bob")
+# print(greeting)
+#
+
+# def greet_person(name,greeting="hello"):
+#     message=f"{greeting},{name}"
+#     return message
+#
+# print(greet_person("alice"))
+# print(greet_person(name:"bob",greeting"hi"))
+#
+
+
+
+
+
+
+
+
