fdc517915c5d2a00b8d3ab3a21ebdbe840718796
Student <ensarsahiti60@gmail.com>
2025-12-18 16:06:08 +0100

    a

---
+++ module13/pandas_series.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+form module13.array_mainpulation import total_sum
+
+producents=['appels','bananas','oranges','drapes','pineapples']
+
+sales=[150,200,180,90,60]
+
+sales_series=pd.series(sales,index=products)
+print(seles_series['grapes'])
+
+print(sales_series)
