b47233f39cefa2f1d655b030e68af178c4f32ee2
Student <ensarsahiti60@gmail.com>
2026-02-10 16:02:39 +0100

    hej

---
+++ module20/main.py
@@ -0,0 +1,74 @@
+#
+
+
+{
+    "name":"john doe",
+    "age":30,
+    "address":{
+
+        "street":"123 main st",
+        "city":"prishtina",
+        "state":"kosovo",
+        "postalcode":"10000"
+    },
+   "contact":[
+
+       {
+           "type":"email",
+           "value":"johundoe@Gmail.com"
+       },
+       {
+           "type":"phone",
+           "value":"444-122-4567."
+                   "0"
+       }
+
+   ]
+
+
+
+    {
+        "fruts":[
+            {
+               "name":"apple",
+                "color":"FF0000";
+                 "details":{
+                     "typr":'pome',
+                     "season":'fall'
+
+                 }
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ]
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
